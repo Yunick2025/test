f7c26834740470fbb02e7faa5fe2e4ab1bf804e8
Yannick Roy <leroydesyannick@gmail.com>
2025-12-06 16:08:36 -0500

    dernier mise a jour

--- test/src/main.jsx
+++ test/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react" ;
+import  ReactDOM  from " react-dom/client" ;
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const elementJSX = <h2>Voici un titre</h2>
+
+ReactDom.createRoot(document.getElementById("root")).render(elementJsx)
