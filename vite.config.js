63eabf3d1a04713175917dce801e518a9a501477
Yannick Roy <leroydesyannick@gmail.com>
2025-12-06 16:37:42 -0500

    dernier mise a jour

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Yunick2025/test.git",
+  base: "/https://github.com/Yunick2025/test.git/",
 })
